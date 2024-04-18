3475b33009d35af4ce02edb461f6bc09929a7315
Ahnheechan <ahc70032@gmail.com>
2024-04-19 01:21:07 +0900

    [Delete] 필요없는 파일 삭제

--- common/check.py
+++
@@ -1,7 +0,0 @@
-from fastapi import Header, Depends
-from database import get_db
-from user import user_service
-
-def get_current_user(token: str = Header(...), db : Session = Depends(get_db)):
-    try:
-        user_email = user_service.ve
